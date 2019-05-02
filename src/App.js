b17c6b55ec1021b35d7282bdf7039506b50d47a5
Tom Tong <573463417@qq.com>
2019-05-02 20:09:59 +1200

    it works now

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+	constructor(){
+		super();
+		this.state={
+			title: ""
+		}
+	}
+
+	componentDidMount(){
+		// let myObject;
+		fetch("https://swapi.co/api/people/1")
+			.then(people => people.json())
+			.then(data => this.setState({title: data['name']}))
+			// .then(data => console.log(data))
+			.catch(err => console.log(err));
+	}
+
+	render(){
+		return (
+			<div>
+				<h1>{this.state.title}</h1>
+			</div>
+		)
+	}	
 }
 
 export default App;
